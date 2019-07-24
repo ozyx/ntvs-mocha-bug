1da6f7507d368d5baa39d2555a194226e2571cbc
Jesse Mazzella <ozyx@users.noreply.github.com>
2019-07-23 20:06:17 -0700

    Initial commit

---
+++ SampleNodeJSJavascript/app.js
@@ -0,0 +1,33 @@
+var assert = require('assert');
+let i = 1;
+
+
+describe('Test Suite 1', function () {
+    it('Test 1', function () {
+        assert.ok(true, "This shouldn't fail");
+    })
+
+    it('Test 2', function () {
+        assert.ok(true, "This shouldn't fail");
+    })
+})
+
+describe('Test Suite 2', function () {
+    it('Test 1', function () {
+        assert.ok(true, "This shouldn't fail");
+    })
+
+    it('Test 2', function () {
+        assert.ok(true, "This shouldn't fail");
+    })
+})
+
+describe('Test Suite 3', function () {
+    it('Test 1', function () {
+        assert.ok(true, "This shouldn't fail");
+    })
+
+    it('Test 2', function () {
+        assert.ok(true, "This shouldn't fail");
+    })
+})
